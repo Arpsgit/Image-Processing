ac3f42b7e0de27e37ec2acfa61dbec281e87b826
Arpan Manna <arpanmanna20@gmail.com>
2022-11-20 00:29:12 +0530

    modify the thresholding.py file

--- thresholding.py
+++ thresholding.py
@@ -3,7 +3,7 @@ with open('panda.pgm', 'r') as picture:
 
 with open('temp.pgm', 'w') as out:
     for i in range(len(element) - 4):
-        if int(element[i+4].replace('\n', '')) < 130: # you can give any threshold value here
+        if int(element[i+4].replace('\n', '')) > 130: # you can give any threshold value here
             element[i+4] = '255\n'
         else:
             element[i+4] = '0\n'
