74d677c09bf072168b0c9af4c1ce5e594e1b3680
Arpan Manna <arpanmanna20@gmail.com>
2022-11-24 13:06:45 +0530

    Update histogram_equalization.py file
    It equalizes the histogram of intensity values of a picture. Made some minor correction

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -5,7 +5,7 @@ frequency = {}
 pdf = {}
 cdf = {}
 mapping = {}
-for i in range(255):
+for i in range(256):
     frequency[f'{i}'] = 0
     pdf[f'{i}'] = 0
     cdf[f'{i}'] = 0
@@ -14,17 +14,17 @@ for i in range(255):
 for i in range(4, len(element)-4):
     frequency[element[i].replace('\n', '')] += 1
 
-for i in range(255):
+for i in range(256):
     pdf[f'{i}'] = round(frequency[f'{i}'] / (len(element)-4), 3)
 
 cdf['0'] = pdf['0']
-for i in range(1, 255):
+for i in range(1, 256):
     cdf[f'{i}'] = cdf[f'{i-1}'] + pdf[f'{i}']
 
-for i in range(255):
+for i in range(256):
     mapping[f'{i}'] = round(255 * cdf[f'{i}'])
 
 with open('temp.pgm', 'w') as out:
     for i in range(4, len(element)-4):
-        element[i] = str(mapping[element[i].replace('\n', '')])
+        element[i] = str(mapping[element[i].replace('\n', '')]) + '\n'
     out.writelines(element)
