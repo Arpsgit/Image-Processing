496154492bd2c3eb6da9e4506af3dfb6ac256825
Arpan Manna <arpanmanna20@gmail.com>
2022-12-26 13:15:03 +0530

    Modify OR.py file
    It'll perform logical OR operation on two pictures

---
+++ Logical_Operations/Or.py
@@ -0,0 +1,23 @@
+with open('Logical_Operations\panda.pgm', 'r') as picture:
+    element1 = picture.readlines()
+
+with open('Logical_Operations\mypic.pgm', 'r') as picture:
+    element2 = picture.readlines()
+
+with open('output3.pgm', 'w') as out:
+    element = ['P2\n','# Made by Arpan\n']
+    element.append(element1[2])
+    element.append('255\n')
+    for i in range(4, len(element1)):
+        if int(element1[i].replace('\n', '')) < int(element2[i].replace('\n', '')):
+            if int(element2[i].replace('\n', '')) < 128:
+                element.append(element1[i])
+            else:
+                element.append(element2[i])
+        else:
+            if int(element1[i].replace('\n', '')) < 128:
+                element.append(element2[i])
+            else:
+                element.append(element1[i])
+
+    out.writelines(element)
