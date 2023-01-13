a90527bc6d1c8403962782402b995eebd7bf0d4d
Arpan Manna <arpanmanna20@gmail.com>
2023-01-13 13:51:00 +0530

    Add power_law.py file

--- PowerLawTransformation/power_law.py
+++ PowerLawTransformation/power_law.py
@@ -5,8 +5,8 @@ name = input("Enter the name of the image file: ")
 with open(f'{name}.pgm', 'r') as picture:
     element = picture.readlines()
 
-gamma = float(input("Enter the gamma value: "))
-C = float(input("Enter the C value: "))
+gamma = float(input("Enter the gamma value: ")) 
+c = float(input("Enter the C value: "))
 
 out_image = input("Enter the name of the output file: ")
 with open(f'{out_image}.pgm', 'w') as out:
@@ -17,5 +17,5 @@ with open(f'{out_image}.pgm', 'w') as out:
         elif pix < 0:
             element[i+4] = '0\n'
         else:
-            element[i+4] = str( int(C * pow(int(element[i+4].replace('\n', '')), gamma))) + '\n'
+            element[i+4] = str( int(c * pow(int(element[i+4].replace('\n', '')), gamma))) + '\n'
     out.writelines(element)
