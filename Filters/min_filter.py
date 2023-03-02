f029d99960affbe6858b573d7ab36e0fbdef8377
Arpan Manna <arpanmanna20@gmail.com>
2023-03-02 20:00:09 +0530

    Edit the file

--- Filters/min_filter.py
+++ Filters/min_filter.py
@@ -4,7 +4,7 @@ input_image = input("File name: ") # enter the name of the image file without ex
 with open(f'{input_image}.pgm', 'r') as picture:
     element = picture.readlines()
 
-def avg_filter(pixel, start, filter_size):
+def min_filter(pixel, start, filter_size):
     new_pixel = np.zeros((pixel.shape[0],pixel.shape[1]))
     for i in range(start, pixel.shape[0]-start):
         for j in range(start, pixel.shape[1]-start):
@@ -23,7 +23,7 @@ for i in range(start, pixel.shape[0]-start):
         pixel[i][j] = int(element[a].replace('\n', ''))
         a += 1
 
-new_pixel = avg_filter(pixel, start, filter_size)
+new_pixel = min_filter(pixel, start, filter_size)
 b = 4
 for i in range(start, new_pixel.shape[0]-start):
         for j in range(start, new_pixel.shape[1]-start):
